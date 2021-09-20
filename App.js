a9b55f332ce9dea40275e1d2b8905f2316f82a15
Joonas Hiltunen <joonas.s.jh@gmail.com>
2021-09-20 15:16:12 +0300

    Revert "Added tab Navigator"
    This reverts commit ff46422b173a974af7d20f400170cc15f7cff667.

--- App.js
+++ App.js
@@ -1,8 +1,29 @@
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import Navigator from './navigators/Navigator';
+import {Platform, SafeAreaView, StyleSheet, View} from 'react-native';
+import List from './components/List';
 
 const App = () => {
-  return <Navigator />;
+  return (
+    <SafeAreaView style={styles.droidSafeArea}>
+      <View style={styles.container}>
+        <List />
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+  },
+
+  droidSafeArea: {
+    flex: 1,
+    backgroundColor: '#ccc',
+    paddingTop: Platform.OS === 'android' ? 30 : 0,
+  },
+});
 
 export default App;
