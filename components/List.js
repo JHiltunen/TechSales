ec55a51457dc7aaa6ee761f856a68ab9814ca44a
m1ole <miikka.huotari@metropolia.fi>
2021-10-06 17:04:08 +0300

    Search

--- components/List.js
+++ components/List.js
@@ -1,14 +1,19 @@
 import React, {useContext, useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
-import {FlatList} from 'react-native';
+import {FlatList, View} from 'react-native';
 import ListItem from './ListItem';
 import {useMedia} from '../hooks/ApiHooks';
 import {MainContext} from '../contexts/MainContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {SearchBar} from 'react-native-elements';
 
 const List = ({navigation}) => {
-  const {mediaArray} = useMedia();
+  let {mediaArray} = useMedia();
   const {update, setUpdate} = useContext(MainContext);
   const [isFetching, setIsFetching] = useState(false);
+  const [search, setSearch] = useState('');
+  const {searchMedia} = useMedia();
+  const [filterMediaArray, setfilterMediaArray] = useState([]);
 
   const refreshList = () => {
     setIsFetching(true);
@@ -20,19 +25,40 @@ const List = ({navigation}) => {
   console.log('List: mediaArray', update);
 
   return (
-    <FlatList
-      data={mediaArray}
-      renderItem={({item}) => (
-        <ListItem
-          singleMedia={item}
-          navigation={navigation}
-          showButtons={false}
-        />
-      )}
-      keyExtractor={(item, index) => index.toString()}
-      onRefresh={refreshList}
-      refreshing={isFetching}
-    />
+    <View>
+      <SearchBar
+        placeholder="Search Here..."
+        onChangeText={async (text) => {
+          setSearch(text);
+
+          try {
+            const token = await AsyncStorage.getItem('userToken');
+            const data = {
+              title: search,
+            };
+            setfilterMediaArray(await searchMedia(token, data));
+            mediaArray = filterMediaArray;
+            console.log('Search result', filterMediaArray);
+          } catch (e) {
+            console.log('Seppo' + e.message);
+          }
+        }}
+        value={search}
+      />
+      <FlatList
+        data={mediaArray}
+        renderItem={({item}) => (
+          <ListItem
+            singleMedia={item}
+            navigation={navigation}
+            showButtons={false}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+        onRefresh={refreshList}
+        refreshing={isFetching}
+      />
+    </View>
   );
 };
 
