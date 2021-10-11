5075d67712130e9d124b75058ccc4a5397f18913
Joonas Hiltunen <joonas.s.jh@gmail.com>
2021-10-11 17:38:51 +0300

    Added lottie animations to login and registration forms

--- views/Login.js
+++ views/Login.js
@@ -7,6 +7,8 @@ import {useUser} from '../hooks/ApiHooks';
 import RegisterForm from '../components/RegisterForm';
 import LoginForm from '../components/LoginForm';
 import {Card, ListItem, Text} from 'react-native-elements';
+import LottieView from 'lottie-react-native';
+import AnimatedLottieView from 'lottie-react-native';
 
 const Login = ({navigation}) => {
   const {setIsLoggedIn, setUser} = useContext(MainContext);
@@ -42,27 +44,45 @@ const Login = ({navigation}) => {
         <Card>
           <Card.Title h4>Register</Card.Title>
           <RegisterForm navigation={navigation} />
+          <ListItem
+            onPress={() => {
+              setRegisterFormToggle(!registerFormToggle);
+            }}
+          >
+            <ListItem.Content>
+              <Text style={styles.text}>Already an account? Login here!</Text>
+            </ListItem.Content>
+            <ListItem.Chevron color="black" />
+          </ListItem>
+          <AnimatedLottieView
+            style={styles.registerAnimations}
+            source={require('../assets/42476-register.json')}
+            autoPlay
+            loop
+          />
         </Card>
       ) : (
         <Card>
           <Card.Title h4>Login</Card.Title>
           <LoginForm navigation={navigation} />
+          <ListItem
+            onPress={() => {
+              setRegisterFormToggle(!registerFormToggle);
+            }}
+          >
+            <ListItem.Content>
+              <Text style={styles.text}>No account ? Register Here!</Text>
+            </ListItem.Content>
+            <ListItem.Chevron color="black" />
+          </ListItem>
+          <AnimatedLottieView
+            style={styles.animations}
+            source={require('../assets/50124-user-profile.json')}
+            autoPlay
+            loop
+          />
         </Card>
       )}
-      <ListItem
-        onPress={() => {
-          setRegisterFormToggle(!registerFormToggle);
-        }}
-      >
-        <ListItem.Content>
-          <Text style={styles.text}>
-            {registerFormToggle
-              ? 'Already an account? Login here!'
-              : 'No account ? Register Here!'}
-          </Text>
-        </ListItem.Content>
-        <ListItem.Chevron />
-      </ListItem>
     </KeyboardAvoidingView>
   );
 };
@@ -71,6 +91,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  animations: {
+    alignSelf: 'center',
+    height: 400,
+  },
+  registerAnimations: {
+    alignSelf: 'center',
+    height: 300,
+  },
+  text: {
+    alignSelf: 'center',
   },
   image: {
     flex: 1,
