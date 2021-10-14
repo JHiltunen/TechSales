069d3ffb931f98a1c2d90ce7c86eb084320ba496
Joonas Hiltunen <joonas.s.jh@gmail.com>
2021-10-14 12:54:17 +0300

    Fixed ios single view

--- views/Single.js
+++ views/Single.js
@@ -4,7 +4,7 @@ import {
   ActivityIndicator,
   TouchableOpacity,
   ScrollView,
-  View,
+  KeyboardAvoidingView,
 } from 'react-native';
 import PropTypes from 'prop-types';
 import {uploadsUrl} from '../utils/variables';
@@ -159,167 +159,172 @@ const Single = ({route}) => {
 
   return (
     <ScrollView>
-      <Card>
-        <ListItem>
-          <Avatar source={{uri: avatar}} />
-          <Text>
-            {ownerInfo.username} || {params.file_id}
-          </Text>
-        </ListItem>
-        <Card.Divider />
-        <Card.Title style={styles.title}>{params.title}</Card.Title>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.container}
+      >
+        <Card>
+          <ListItem>
+            <Avatar source={{uri: avatar}} />
+            <Text>
+              {ownerInfo.username} || {params.file_id}
+            </Text>
+          </ListItem>
+          <Card.Divider />
+          <Card.Title style={styles.title}>{params.title}</Card.Title>
 
-        {params.media_type === 'image' && (
-          <Card.Image
-            source={{uri: uploadsUrl + params.filename}}
-            style={styles.image}
-            PlaceholderContent={<ActivityIndicator />}
-          />
-        )}
-        {params.media_type === 'video' && (
-          <TouchableOpacity // usePoster hides video so use this to start it
-            disabled={disabled}
-            onPress={() => {
-              videoRef.playAsync();
-              setDisabled(true); // disable touchableOpacity when video is started
-            }}
-          >
-            <Video
-              ref={handleVideoRef}
-              style={styles.image}
-              source={{uri: uploadsUrl + params.filename}}
-              useNativeControls
-              resizeMode="contain"
-              usePoster
-              posterSource={{uri: uploadsUrl + params.screenshot}}
-            />
-          </TouchableOpacity>
-        )}
-        {params.media_type === 'audio' && (
-          <>
-            <Text>Audio not supported YET.</Text>
-            <Audio></Audio>
-          </>
-        )}
-        <ListItem>
           {params.media_type === 'image' && (
-            <Icon name="image-outline" type="ionicon" />
+            <Card.Image
+              source={{uri: uploadsUrl + params.filename}}
+              style={styles.image}
+              PlaceholderContent={<ActivityIndicator />}
+            />
           )}
           {params.media_type === 'video' && (
-            <Icon name="videocam-outline" type="ionicon" />
+            <TouchableOpacity // usePoster hides video so use this to start it
+              disabled={disabled}
+              onPress={() => {
+                videoRef.playAsync();
+                setDisabled(true); // disable touchableOpacity when video is started
+              }}
+            >
+              <Video
+                ref={handleVideoRef}
+                style={styles.image}
+                source={{uri: uploadsUrl + params.filename}}
+                useNativeControls
+                resizeMode="contain"
+                usePoster
+                posterSource={{uri: uploadsUrl + params.screenshot}}
+              />
+            </TouchableOpacity>
           )}
-          <ListItem.Content>
-            <ListItem.Subtitle style={styles.date}>
-              {formatDate(new Date(params.time_added), 'eee d. MMM y')} klo.
-              {formatDate(new Date(params.time_added), 'HH.mm')}
-            </ListItem.Subtitle>
-          </ListItem.Content>
-        </ListItem>
+          {params.media_type === 'audio' && (
+            <>
+              <Text>Audio not supported YET.</Text>
+              <Audio></Audio>
+            </>
+          )}
+          <ListItem>
+            {params.media_type === 'image' && (
+              <Icon name="image-outline" type="ionicon" />
+            )}
+            {params.media_type === 'video' && (
+              <Icon name="videocam-outline" type="ionicon" />
+            )}
+            <ListItem.Content>
+              <ListItem.Subtitle style={styles.date}>
+                {formatDate(new Date(params.time_added), 'eee d. MMM y')} klo.
+                {formatDate(new Date(params.time_added), 'HH.mm')}
+              </ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
 
-        <ListItem style={styles.container}>
-          <ListItem.Title style={styles.listItemTitle}>
-            Description:
-          </ListItem.Title>
-          <ListItem.Content style={styles.listItemContent}>
-            <Text style={styles.text}>{allData.description}</Text>
-          </ListItem.Content>
-        </ListItem>
-        <ListItem style={styles.container}>
-          <ListItem.Title style={styles.listItemTitle}>
-            Condition:
-          </ListItem.Title>
-          <ListItem.Content style={styles.listItemContent}>
-            <Text style={styles.text}>{allData.condition}</Text>
-          </ListItem.Content>
-        </ListItem>
-        <ListItem style={styles.container}>
-          <ListItem.Title style={styles.listItemTitle}>Price:</ListItem.Title>
-          <ListItem.Content style={styles.listItemContent}>
-            <Text style={styles.text}>{allData.price} €</Text>
-          </ListItem.Content>
-        </ListItem>
-        <Card.Divider />
+          <ListItem style={styles.container}>
+            <ListItem.Title style={styles.listItemTitle}>
+              Description:
+            </ListItem.Title>
+            <ListItem.Content style={styles.listItemContent}>
+              <Text style={styles.text}>{allData.description}</Text>
+            </ListItem.Content>
+          </ListItem>
+          <ListItem style={styles.container}>
+            <ListItem.Title style={styles.listItemTitle}>
+              Condition:
+            </ListItem.Title>
+            <ListItem.Content style={styles.listItemContent}>
+              <Text style={styles.text}>{allData.condition}</Text>
+            </ListItem.Content>
+          </ListItem>
+          <ListItem style={styles.container}>
+            <ListItem.Title style={styles.listItemTitle}>Price:</ListItem.Title>
+            <ListItem.Content style={styles.listItemContent}>
+              <Text style={styles.text}>{allData.price} €</Text>
+            </ListItem.Content>
+          </ListItem>
+          <Card.Divider />
 
-        <ListItem>
-          {/* TODO: show like or dislike button depending on the current like status,
+          <ListItem>
+            {/* TODO: show like or dislike button depending on the current like status,
           calculate like count for a file */}
-          {!iAmLikingIt ? (
-            <AntIcon
-              name="hearto"
-              color="black"
-              size={25}
-              onPress={async () => {
-                const token = await AsyncStorage.getItem('userToken');
-                const likePost = await addFavourite(params.file_id, token);
-                if (likePost) {
-                  getLikes();
-                }
-                console.log('Like post:', likePost);
-              }}
-            />
-          ) : (
-            <AntIcon
-              name="heart"
-              color="red"
-              size={25}
-              onPress={async () => {
-                const token = await AsyncStorage.getItem('userToken');
-                const dontLikePost = await deleteFavourite(
-                  params.file_id,
-                  token
-                );
-                if (dontLikePost) {
-                  getLikes();
-                }
-                console.log('dont like post:', dontLikePost);
-              }}
-            />
-          )}
-          <Text>Likes: {likes.length}</Text>
-        </ListItem>
-        <Card.Divider />
-        <Input
-          placeholder="Comment"
-          onChangeText={(value) => {
-            setComment(value);
-          }}
-          leftIcon={<Icon name="comment" size={24} color="black" />}
-          rightIcon={
-            <Icon
-              name="send"
-              size={24}
-              color="black"
-              onPress={async () => {
-                try {
+            {!iAmLikingIt ? (
+              <AntIcon
+                name="hearto"
+                color="black"
+                size={25}
+                onPress={async () => {
                   const token = await AsyncStorage.getItem('userToken');
-                  const commentObject = {
-                    file_id: params.file_id,
-                    comment: comment,
-                  };
-                  console.log('comment:', commentObject);
-                  const upload = await uploadComment(
-                    JSON.stringify(commentObject),
+                  const likePost = await addFavourite(params.file_id, token);
+                  if (likePost) {
+                    getLikes();
+                  }
+                  console.log('Like post:', likePost);
+                }}
+              />
+            ) : (
+              <AntIcon
+                name="heart"
+                color="red"
+                size={25}
+                onPress={async () => {
+                  const token = await AsyncStorage.getItem('userToken');
+                  const dontLikePost = await deleteFavourite(
+                    params.file_id,
                     token
                   );
-                  if (upload) {
-                    setCommentUpdate(commentUpdate + 1);
-                    alert('Comment added');
+                  if (dontLikePost) {
+                    getLikes();
                   }
-                } catch (e) {
-                  alert('Error: ', e.message);
-                  console.log('Error', e.message);
-                }
-              }}
-            />
-          }
-        />
-        {comments
-          .map((c, index = comments.length - 1 - index) => {
-            console.log('kommentti', c);
-            return <Comment key={index} comment={c} />;
-          })
-          .reverse()}
-      </Card>
+                  console.log('dont like post:', dontLikePost);
+                }}
+              />
+            )}
+            <Text>Likes: {likes.length}</Text>
+          </ListItem>
+          <Card.Divider />
+          <Input
+            placeholder="Comment"
+            onChangeText={(value) => {
+              setComment(value);
+            }}
+            leftIcon={<Icon name="comment" size={24} color="black" />}
+            rightIcon={
+              <Icon
+                name="send"
+                size={24}
+                color="black"
+                onPress={async () => {
+                  try {
+                    const token = await AsyncStorage.getItem('userToken');
+                    const commentObject = {
+                      file_id: params.file_id,
+                      comment: comment,
+                    };
+                    console.log('comment:', commentObject);
+                    const upload = await uploadComment(
+                      JSON.stringify(commentObject),
+                      token
+                    );
+                    if (upload) {
+                      setCommentUpdate(commentUpdate + 1);
+                      alert('Comment added');
+                    }
+                  } catch (e) {
+                    alert('Error: ', e.message);
+                    console.log('Error', e.message);
+                  }
+                }}
+              />
+            }
+          />
+          {comments
+            .map((c, index = comments.length - 1 - index) => {
+              console.log('kommentti', c);
+              return <Comment key={index} comment={c} />;
+            })
+            .reverse()}
+        </Card>
+      </KeyboardAvoidingView>
     </ScrollView>
   );
 };
